f119ba0902f0ebf1451805a765e8ef9f8d0ac27a
GitHub Action <action@github.com>
2021-08-21 05:04:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kinglongchen/kinglongideapluginone/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kinglongchen.kinglongideapluginone.services
+
+import com.github.kinglongchen.kinglongideapluginone.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kinglongchen/kinglongideapluginone/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kinglongchen/kinglongideapluginone/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kinglongchen.kinglongideapluginone.services
 
+import com.github.kinglongchen.kinglongideapluginone.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
